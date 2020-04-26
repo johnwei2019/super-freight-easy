7269ddc2873d1ab1a1864b2ea644e27bedc2f01b
John Wei <john.jichao.wei@gmail.com>
2020-04-26 14:22:39 +1000

    Add load store config function

---
+++ app/load_store_config.js
@@ -0,0 +1,24 @@
+const yaml = require('js-yaml');
+const S3Client = require('./s3_client')
+
+const createStoreConfig = ({
+  storeId, storeName, address1, address2, suburb, postcode, state, phone, email
+}) => ({
+  storeId,
+  storeName,
+  address1,
+  address2,
+  suburb,
+  postcode,
+  state,
+  phone,
+  email
+});
+
+module.exports = async (userId, storeId, s3Client = new S3Client()) => {
+  const bucket = process.env.STORE_CONFIGS_BUCKET
+  const key = `${userId}/${storeId}/super_freight_config.yml`
+  const configContent = await s3Client.get(bucket, key)
+  const data = yaml.safeLoad(configContent)
+  return createStoreConfig(data)
+}
---
+++ app/tests/load_store_config_test.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const chai = require('chai');
+const expect = chai.expect;
+const sinon = require('sinon')
+const loadStoreConfig = require('../load_store_config');
+const sample_data = require('fs').readFileSync('./tests/fixtures/store_config_sample.yaml').toString();
+const bucket = 'dummy-bucket'
+const userId = 'dummy-user-id';
+const storeId = 'dummy-store-id';
+const s3Client = {
+  get: function() {
+    return Promise.resolve(sample_data);
+  }
+};
+const expectedConfig = {
+  storeId: "dummy_store_id",
+  storeName: "Dummy Store",
+  address1: "Unit 1",
+  address2: "Supernice Road",
+  suburb: "Blackswan",
+  postcode: "3100",
+  state: "VIC",
+  phone: "0436888999",
+  email: "someone@gmail.com"
+}
+
+describe('loadStoreConfig', function() {
+  beforeEach(function() {
+    process.env.STORE_CONFIGS_BUCKET = bucket
+  })
+
+  afterEach(function() {
+    delete process.env.STORE_CONFIGS_BUCKET
+  })
+
+  it('load store config from s3', async function() {
+    const spy = sinon.spy(s3Client, 'get')
+    await loadStoreConfig(userId, storeId, s3Client)
+    expect(spy.calledOnceWith(
+      bucket, 'dummy-user-id/dummy-store-id/super_freight_config.yml'
+    )).to.be.true
+  });
+
+  it('returns the store config data object', async function() {
+    const config = await loadStoreConfig(userId, storeId, s3Client)
+    expect(config).to.deep.equal(expectedConfig)
+  });
+});
